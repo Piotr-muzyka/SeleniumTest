e90e6a1e8b70dac4252753b352e012c82141cf67
root <root@localhost.localdomain>
2017-04-09 15:10:41 -0400

    fix, fall back to simple test

--- src/test/java/simpleTest/firstTest.java
+++ src/test/java/simpleTest/firstTest.java
@@ -5,38 +5,17 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.junit.Assert; 
 
-import java.io.File;
-import java.io.IOException;
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-
 public class firstTest {
 
 	@Test
 	public void startWebDriver()
-{
-	// Open Firefox
-	 WebDriver driver=new FirefoxDriver();
-
-	// Maximize the window
-	driver.manage().window().maximize();
-
-	// Pass the url
-	driver.get("http://www.google.com");
-
-	// Take screenshot and store as a file format
-	File src= ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-	try {
-	 // now copy the  screenshot to desired location using copyFile //method
-	FileUtils.copyFile(src, new File("C:/selenium/error.png"));
+	{
+		System.setProperty("webdriver.gecko.driver","gecko/geckodriver");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.google.com");		
+		//driver.navigate().to("http://www.google.com/");
+		Assert.assertTrue("Is title correct", driver.getTitle().startsWith("Google"));
+		//driver.getScreenshotAs("screenie.png");
+		driver.close();
 	}
-
-	catch (IOException e)
-	 {
-	  System.out.println(e.getMessage());
-
-	 }
-}
+} 
