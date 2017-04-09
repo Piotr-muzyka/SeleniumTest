a9f73cf588ad2202681193ee46c029b353968913
root <piotrmuzyka@yahoo.com>
2017-04-09 16:18:46 -0400

    fix, added screenshot functionality

--- src/test/java/simpleTest/firstTest.java
+++ src/test/java/simpleTest/firstTest.java
@@ -5,17 +5,41 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.junit.Assert; 
 
+import java.io.File;
+import java.io.IOException;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+
+
+
 public class firstTest {
 
 	@Test
 	public void startWebDriver()
 	{
-		System.setProperty("webdriver.gecko.driver","gecko/geckodriver");
-		WebDriver driver = new FirefoxDriver();
-		driver.get("https://www.google.com");		
-		//driver.navigate().to("http://www.google.com/");
-		Assert.assertTrue("Is title correct", driver.getTitle().startsWith("Google"));
-		//driver.getScreenshotAs("screenie.png");
-		driver.close();
+	// Open Firefox
+	System.setProperty("webdriver.gecko.driver","gecko/geckodriver");
+	 WebDriver driver=new FirefoxDriver();
+
+	// Maximize the window
+	driver.manage().window().maximize();
+
+	// Pass the url
+	driver.get("http://www.google.com");
+
+	// Take screenshot and store as a file format
+	File src= ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+	try{
+	// now copy the  screenshot to desired location using copyFile //method
+	FileUtils.copyFile(src, new File("/root/mygit/Test/SeleniumTest/error.png"));
 	}
-} 
+	catch(IOException e)
+	{
+	System.out.println(e.getMessage());
+	}
+	driver.close();
+	
+	}
+}
